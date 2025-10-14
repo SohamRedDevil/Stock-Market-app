529b545e93664a92a52b434b2f213fdaa1e89f19
SohamRedDevil <33813936+SohamRedDevil@users.noreply.github.com>
2025-10-14 13:52:31 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 strategy_params = {
     "MA": {"fast": [10, 20], "slow": [50, 100]},
     "RSI": {"window": [14], "overbought": [70], "oversold": [30]},
-    "MACD": {"fast": [12], "slow": [26], "signal": [9]},
+    "MACD": {"fast_window": [12], "slow_window": [26], "signal_window": [9]},
     "Bollinger": {"window": [20], "std": [2]},
     "Breakout": {"window": [20]},
     "Momentum": {"window": [10]},
